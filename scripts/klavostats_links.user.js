ac40ed6d8e4940aca95e40ae1b5239287860c2cd
Vitaliy <vitbusko@gmail.com>
2015-07-31 20:40:47 +0600

    fix bug #56

--- scripts/klavostats_links.user.js
+++ scripts/klavostats_links.user.js
@@ -1,30 +1,48 @@
 ﻿// ==UserScript==
 // @name          Klavogonki: klavostats links
 // @namespace     klavogonki
-// @version       2.1 KTS
+// @version       2.2
 // @description   Добавляет прямые ссылки на профиль игрока в КлавоСтатистике
 // @include       http://klavogonki.ru/u/*
 // @author        Lexin
+// @contributors  Fenex
 // @updateURL     
 // @downloadURL   
 // ==/UserScript==
 
-function main(){
-	function fn() {
-		var menu = jQuery('.sidebar');
-		if (menu.length) {
-			var player = jQuery('.profile-header .name').clone().children().remove().end().text();
-			player = jQuery.trim(player);
-			var group = jQuery('<ul class="profile-nav"></ul>');
-			group.append(jQuery('<li><a href="http://stat.klavogonki.ru/players.php?extra&n=' + player + '">КлавоСтатистика</a></li>'));
-			group.append(jQuery('<li><a href="http://stat.klavogonki.ru/history.php#' + player + '">История игрока</a></li>'));
-			group.append(jQuery('<li><a href="http://klavogonki.ru/vocs/search?section=all&type=all&order=&changed=&searchtext=%D0%B0%D0%B2%D1%82%D0%BE%D1%80%3A' + player + '">Созданные словари</a></li>'));
-			menu.append(group);
-		} else {
-			setTimeout(fn, 500);
-		}
-	}
-	setTimeout(fn, 500);
+function main() {
+	setInterval(function() {
+        var menu = angular.element('.sidebar');
+        
+        if(!menu.length) return;
+        if(menu.find('.klavostat-links').length) return;
+        
+        var player = angular.element('profile-header .name').contents().first().text().trim();
+        var group = angular.element('<ul class="profile-nav klavostat-links"></ul>');
+        
+        group.append( angular.element(
+            '<li>\
+                <a href="http://stat.klavogonki.ru/players.php?extra&n=' + player + '">КлавоСтатистика</a>\
+            </li>'
+        ) );
+        
+        group.append( angular.element(
+            '<li>\
+                <a href="http://stat.klavogonki.ru/history.php#' + player + '">История игрока</a>\
+            </li>'
+        ) );
+        
+        group.append( angular.element(
+            '<li>\
+                <a href="http://klavogonki.ru/vocs/search?section=all&type=all&order=&changed=&searchtext=%D0%B0%D0%B2%D1%82%D0%BE%D1%80%3A' + player + '">\
+                    Созданные словари\
+                </a>\
+            </li>'
+        ) );
+        
+        menu.append(group);
+        
+    }, 500);
 }
 
 function execScript(source) {
@@ -38,11 +56,4 @@ function execScript(source) {
 	document.body.appendChild(script);
 }
 
-if(!document.getElementById('KTS_klavostats_links')) {
-	execScript(main);
-
-	var tmp_elem = document.createElement('div');
-	tmp_elem.id = 'KTS_klavostats_links';
-	tmp_elem.style.display = 'none';
-	document.body.appendChild(tmp_elem);	
-}
+execScript(main);
