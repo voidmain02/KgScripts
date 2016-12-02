5e491e17d6fcef87d1358b3e7bdf3b9dc7bf4863
Daniil <filippovdaniil@gmail.com>
2016-12-02 23:36:21 +0700

    (f) Fixed the DelGameButton script. Related to #91

--- scripts/DelGameButton.user.js
+++ scripts/DelGameButton.user.js
@@ -2,44 +2,77 @@
 // @name           DelGameButton
 // @namespace      klavogonki
 // @include        http://klavogonki.ru/g/*
-// @author         Fenex
+// @author         Fenex, agile
 // @description    Кнопка удаления результата в соревнованиях (работает только для Премиум-аккаунтов)
-// @version        2.1.1
+// @version        2.1.2
 // @icon           http://www.gravatar.com/avatar.php?gravatar_id=d9c74d6be48e0163e9e45b54da0b561c&r=PG&s=48&default=identicon
 // ==/UserScript==
-function checkFinished(youid) {
-	if(game.players[youid].info.finished!=false) {
-		clearInterval(fnxtm);
-		var el = document.getElementById('place'+youid);
-		if ((game.params.competition!=0)&&(game.params.competition!=null)) {
-			var str = document.createElement('div');
-			str.setAttribute('class', 'delresult');
-			str.innerHTML = ' <a original-title="Отменить результат (только для Премиум)" href="javascript:game.delresult();"><img src="/img/delete-16.gif"></a>';
-			el.parentNode.insertBefore(str, el.nextSibling);
-		}
-	}
-}
-function chek_game() {
-	if(game && game.params && game.players) {
-		clearInterval(fnxtm);
-		var youid;
-		for(i=0;i<game.players.length;i++) {
-			if((game.players[i].you=='you')||(game.players[i].you==true)) {
-				youid = game.players[i].info.id;
-				break;
-			}
-		}
-		fnxtm = setInterval('checkFinished('+youid+')', 1000);
-	}
+
+function delGameButton () {
+
+  function deleteResult (gameId, playerRow) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', '/g/' + gameId + '.delresult');
+    xhr.onload = function () {
+      if (this.status !== 200) {
+        throw new Error('Something went wrong while deleting the game result.');
+      }
+
+      playerRow.querySelector('.stats').textContent = 'Результат отменён.';
+    }
+    xhr.send();
+  }
+
+  function createButton (playerRow) {
+    // Check the non-rating competition case:
+    if (playerRow.querySelector('.delresult')) {
+      return false;
+    }
+
+    var place = playerRow.querySelector('.rating .place');
+    if (!place) {
+      throw new Error('.player.you .rating .place not found.');
+    }
+
+    var gameIdMatches = window.location.href.match(/\?gmid=(\d+)/);
+    if (!gameIdMatches) {
+      throw new Error('Can\'t get the game id from the URL');
+    }
+
+    var gameId = gameIdMatches[1];
+    var container = document.createElement('div');
+    container.classList.add('delresult');
+    // Adjusting position because the .rating_gained element will overlap us:
+    container.style.left = '0';
+    container.style.top = '-20px';
+    var link = document.createElement('a');
+    link.title = 'Отменить результат (только для Премиум)';
+    link.addEventListener('click', deleteResult.bind(null, gameId, playerRow));
+    var img = document.createElement('img');
+    img.src = '/img/delete-16.gif';
+    link.appendChild(img);
+    container.appendChild(link);
+    place.parentNode.insertBefore(container, place.nextSibling);
+  }
+
+  var scores = document.getElementById('userpanel-scores');
+  if (!scores) {
+    throw new Error('#userpanel-scores not found.');
+  }
+
+  var observer = new MutationObserver(function () {
+    var finished = document.querySelector('.player.you .rating');
+    // Are we finished the race yet?
+    if (!finished || finished.style.display === 'none') {
+      return false;
+    }
+
+    observer.disconnect();
+    return createButton(document.querySelector('.player.you'));
+  });
+  observer.observe(scores, { childList: true });
 }
 
-if(!document.getElementById('KTS_DelGameButton')) {
-	var s = document.createElement('script');
-	s.innerHTML = chek_game+checkFinished+"var fnxtm = setInterval('chek_game()', 1000);";
-	document.body.appendChild(s);
-
-	var tmp_elem = document.createElement('div');
-	tmp_elem.id = 'KTS_DelGameButton';
-	tmp_elem.style.display = 'none';
-	document.body.appendChild(tmp_elem);	
-}
+var script = document.createElement('script');
+script.textContent = '(' + delGameButton.toString() + ')();';
+document.body.appendChild(script);
