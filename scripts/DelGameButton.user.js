41c1dfbace0160be724afa4661acb0dc51baf96d
Daniil <filippovdaniil@gmail.com>
2016-12-11 14:24:05 +0700

    (f) Fix for a Chrome issue with the DelGameButton script

--- scripts/DelGameButton.user.js
+++ scripts/DelGameButton.user.js
@@ -4,7 +4,7 @@
 // @include        http://klavogonki.ru/g/*
 // @author         Fenex, agile
 // @description    Кнопка удаления результата в соревнованиях (работает только для Премиум-аккаунтов)
-// @version        2.1.2
+// @version        2.1.3
 // @icon           http://www.gravatar.com/avatar.php?gravatar_id=d9c74d6be48e0163e9e45b54da0b561c&r=PG&s=48&default=identicon
 // ==/UserScript==
 
@@ -70,7 +70,11 @@ function delGameButton () {
     observer.disconnect();
     return createButton(document.querySelector('.player.you'));
   });
-  observer.observe(scores, { childList: true });
+  observer.observe(scores, {
+    childList: true,
+    characterData: true,
+    subtree: true,
+  });
 }
 
 var script = document.createElement('script');
