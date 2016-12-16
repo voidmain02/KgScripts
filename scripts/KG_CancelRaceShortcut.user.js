6e148e32a09dfe91ccc78a72ebb112e5d8113864
Daniil <filippovdaniil@gmail.com>
2016-12-16 21:41:07 +0700

    (f) Fixed KG_CancelRaceShortcut userscript. Related to #91

--- scripts/KG_CancelRaceShortcut.user.js
+++ scripts/KG_CancelRaceShortcut.user.js
@@ -1,40 +1,49 @@
 // ==UserScript==
 // @name        KG_CancelRaceShortcut
 // @namespace   klavogonki
-// @description Добавляет сочетание клавиш для отмены результатов заезда
+// @description Добавляет сочетание Ctrl + Shift + Z для отмены результатов заезда
 // @author      voidmain, unnamed777, novkostya
 // @license     MIT
-// @version     1.1.0
+// @version     1.1.1
 // @include     http://klavogonki.ru/g/*
 // @grant       none
-// @run-at      document-end 
+// @run-at      document-end
 // ==/UserScript==
 
+function cancelRaceShortcut() {
+  function deleteResult (gameId, playerStats) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', '/g/' + gameId + '.delresult');
+    xhr.onload = function () {
+      if (this.status !== 200) {
+        throw new Error('Something went wrong while deleting the game result.');
+      }
 
-function exec(fn) {
-    var script = document.createElement('script');
-    script.setAttribute("type", "application/javascript");
-    script.textContent = '(' + fn + ')();';
-    document.body.appendChild(script); // run the script
-    document.body.removeChild(script); // clean up
-}
+      playerStats.textContent = 'Результат отменён.';
+    };
+    xhr.send();
+  }
 
-function main() {
-    var playerIndex = null;
-    for(var i = 0; i < game.players.length; i++) {
-        if(game.players[i].you) {
-            playerIndex = i;
-            break;
-        }
+  // Extract the game id from the URL:
+  var matches = window.location.href.match(/\/\/klavogonki.ru\/g\/\?gmid=(\d+)/);
+  if (!matches) {
+      throw new Error('game id was not parsed.');
+  }
+
+  document.addEventListener('keydown', function(event) {
+    // Shift + Ctrl + Z:
+    if(event.shiftKey && event.ctrlKey && event.keyCode == 90) {
+      var finished = document.querySelector('.player.you .rating');
+      // Are we finished the race yet?
+      if (!finished || finished.style.display === 'none') {
+        return false;
+      }
+
+      deleteResult(matches[1], document.querySelector('.player.you .stats'));
     }
-    document.addEventListener('keydown', function(e) {
-        if(e.shiftKey && e.ctrlKey && e.keyCode == 90 && game.players[playerIndex].info.finished) {
-            game.delresult();
-        }
-    }, false);
+  }, false);
 }
 
-window.addEventListener("load", function() {
-    // script injection
-    exec(main);
-}, false);
+var script = document.createElement('script');
+script.textContent = '(' + cancelRaceShortcut.toString() + ')();';
+document.body.appendChild(script);
