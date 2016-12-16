43f1f5757551f9170d9c98893eefba88de697027
Daniil <filippovdaniil@gmail.com>
2016-12-16 15:09:26 +0700

    (f) Fixed the KG_TimeoutAlert userscript. Related to #91

--- scripts/KG_TimeoutAlert.user.js
+++ scripts/KG_TimeoutAlert.user.js
@@ -1,37 +1,50 @@
 // ==UserScript==
 // @name        KG_TimeoutAlert
 // @author      Andre_Macareno, Fenex, un4given, agile
-// @version     3.0.2
+// @version     3.0.3
 // @description Звуковое оповещение о старте игры
 // @include     http://klavogonki.ru/g/*
 // ==/UserScript==
 
-function main() {
-    if (game.params.timeout < 45) {
-        return;
+function timeoutAlert() {
+  if (!soundManager) {
+    throw new Error('soundManager instance not found.');
+  }
+
+  var desc = document.getElementById('gamedesc');
+  if (!desc) {
+      throw new Error('#gamedesc element not found.');
+  }
+
+  var matches = desc.textContent.match(/таймаут\s(\d+)\s(сек|мин)/);
+  // No alert if the game timeout less than 45 seconds:
+  if (matches[1] < 45 && matches[2] === 'сек') {
+    return false;
+  }
+
+  var timeContainer = document.getElementById('waiting_timeout');
+  if (!timeContainer) {
+    throw new Error('#waiting_timeout element not found.');
+  }
+
+  soundManager.createSound('timeoutAlert', 'http://klavogonki.ru/typo.mp3');
+
+  var timer = setInterval(function() {
+    var remainingTime = timeContainer.textContent.match(/(\d{2}).(\d{2})/);
+    if (!remainingTime) {
+      throw new Error('remaining time was not parsed.');
     }
 
-    soundManager.createSound('to_alert', 'http://klavogonki.ru/typo.mp3');
-
-    game.to_alert_timer = setInterval(function() {
-        var time = document.getElementById('waiting_timeout').innerHTML.match(/(\d{2}).(\d{2})/);
-        if (!time || time.length!=3) {
-            return;
-        }
-        var sec = parseInt(time[2]);
-        var min = parseInt(time[1]);
-        if (min == 0 && sec < 18) {
-            if (game.to_alert_timer) {
-                clearInterval(game.to_alert_timer);
-            }
-            soundManager.play('to_alert');
-        }
-    }, 500);
+    // Make a sound alert in ≈ 17 seconds before the game start:
+    if (parseInt(remainingTime[1]) === 0 && parseInt(remainingTime[2]) < 18) {
+      clearInterval(timer);
+      soundManager.play('timeoutAlert');
+    }
+  }, 500);
 }
 
-window.addEventListener('load', function() {
-    var inject = document.createElement('script');
-    inject.setAttribute('type', 'application/javascript');
-    inject.appendChild(document.createTextNode('('+main.toString()+')()'));
-    document.body.appendChild(inject);
+window.addEventListener('load', function () {
+  var inject = document.createElement('script');
+  inject.appendChild(document.createTextNode('(' + timeoutAlert.toString() + ')()'));
+  document.body.appendChild(inject);
 });
