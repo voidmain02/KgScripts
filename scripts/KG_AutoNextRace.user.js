d039110955bee19833732d62758d4baea3a9033e
Phemmer <P-h-e-m-m-e-r@users.noreply.github.com>
2017-04-29 00:40:02 +0300

    change to MutationObserver

--- scripts/KG_AutoNextRace.user.js
+++ scripts/KG_AutoNextRace.user.js
@@ -1,64 +1,61 @@
 // ==UserScript==
 // @name         KG_AutoNextRace
 // @namespace    klavogonki
-// @version      1.2.1
+// @version      1.3.0
 // @description  Добавляет возможность автоматического старта заездов и создания следующего после набора
 // @author       Phemmer
 // @include      http://klavogonki.ru/g/*
 // ==/UserScript==
 
-var interval_auto_next_check = 0;
-function chbChanged() {
-	console.log(interval_auto_next_check);
-	var param = document.getElementById("auto_next_check");
-	if (param.checked) 	{
-		localStorage.autoNext_STATUS = '1';
-		game.hostStart();
-		interval_auto_next_check = setInterval(CheckArrive, 500);
-	}
-	else {
-		localStorage.autoNext_STATUS = '0';
-		clearInterval(interval_auto_next_check);
-	}
-}
+var isCompetition = document.getElementById('gamedesc').innerText.match(/Обычный, соревнование/) !== null;
+if (isCompetition) return;
 
-function CheckArrive()
-{
-	if (document.title[0] == '[' && localStorage.autoNext_STATUS == '1')
-	{
-		modeName = document.querySelector(".you");
-		if (modeName !== null && modeName.innerText.match(' место ') !== null)
-		{
-			setTimeout(GoToNext, 1500);
-		}
-	}
-}
-
-function GoToNext() {
-	window.location='/g/' + document.location.href.substr(29, 5) + '.replay';
-}
-
-
-var IsCompetition = document.getElementById('gamedesc').innerText.match(/Обычный, соревнование/) !== null;
-if (IsCompetition) return;
-
+var chbAutoNext;
+var waitingForNext = false;
 var params = document.getElementById("param_shadow");
 if (params) {
 	var elem = document.createElement("div");
 	elem.id = "auto_next";
-	elem.innerHTML = '<input id="auto_next_check" type="checkbox"><label for="auto_next_check">Автоматически играть еще раз</label>' + '<input type="hidden" id="auto_next_flag">';
+	elem.innerHTML = '<input id="auto_next_check" type="checkbox"><label for="auto_next_check">Автоматически играть еще раз</label><input type="hidden" id="auto_next_flag">';
 	params.parentNode.insertBefore(elem, params);
-	document.getElementById("auto_next_check").addEventListener("click", chbChanged, false);
+	chbAutoNext = document.getElementById("auto_next_check");
+	chbAutoNext.addEventListener("click", chbChanged, false);
+
+	if (localStorage.autoNext_STATUS == '1')
+	{
+		chbAutoNext.checked = 1;
+		game.hostStart();
+	}
 }
 
-var script = document.createElement("script");
-script.innerHTML = chbChanged + CheckArrive + GoToNext;
-document.body.appendChild(script);
-
-var param = document.getElementById("auto_next_check");
-
-if (localStorage.autoNext_STATUS == '1')
-{
-    param.checked = 1;
-    chbChanged();
+function chbChanged() {
+	if (chbAutoNext.checked) 	{
+		localStorage.autoNext_STATUS = '1';
+		game.hostStart();
+		CheckRating();
+	}
+	else {
+		localStorage.autoNext_STATUS = '0';
+		waitingForNext = false;
+	}
 }
+
+document.getElementById("userpanel-scores-container").addEventListener("DOMSubtreeModified", CheckRating);
+
+function CheckRating(){
+	var rating = document.querySelector(".player.you .rating");
+	if (localStorage.autoNext_STATUS == '1' &&
+		rating !== null && rating.innerText.match(' место ') !== null &&
+		waitingForNext === false)
+	{
+		waitingForNext = true;
+		setTimeout(GoToNext, 2000);
+	}
+}
+
+function GoToNext() {
+	if (localStorage.autoNext_STATUS == '1')
+		window.location='/g/' + document.location.href.substr(29, 5) + '.replay';
+}
+
+
