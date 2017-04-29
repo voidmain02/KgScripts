1c4c7e8c9b8e3425f27ee245e1fd84fecc98aa96
Phemmer <P-h-e-m-m-e-r@users.noreply.github.com>
2017-04-29 16:39:29 +0300

    changed into MutationObserver

--- scripts/KG_AutoNextRace.user.js
+++ scripts/KG_AutoNextRace.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         KG_AutoNextRace
 // @namespace    klavogonki
-// @version      1.3.0
-// @description  Добавляет возможность автоматического старта заезда и автосоздания следующего после набора
+// @version      1.4.0
+// @description  Добавляет возможность автоматического старта заездов и создания следующего после набора
 // @author       Phemmer
 // @include      http://klavogonki.ru/g/*
 // ==/UserScript==
@@ -10,24 +10,36 @@
 var isCompetition = document.getElementById('gamedesc').innerText.match(/Обычный, соревнование/) !== null;
 if (isCompetition) return;
 
-var chbAutoNext;
-var waitingForNext = false;
 var params = document.getElementById("param_shadow");
-if (params) {
-	var elem = document.createElement("div");
-	elem.id = "auto_next";
-	elem.innerHTML = '<input id="auto_next_check" type="checkbox"><label for="auto_next_check">Автоматически играть еще раз</label><input type="hidden" id="auto_next_flag">';
-	params.parentNode.insertBefore(elem, params);
-	chbAutoNext = document.getElementById("auto_next_check");
-	chbAutoNext.addEventListener("click", chbChanged, false);
+var elem = document.createElement("div");
+elem.id = "auto_next";
+elem.innerHTML = '<input id="auto_next_check" type="checkbox"><label for="auto_next_check">Автоматически играть еще раз</label>';
+params.parentNode.insertBefore(elem, params);
+var chbAutoNext = document.getElementById("auto_next_check");
+chbAutoNext.addEventListener("click", chbChanged, false);
 
-	if (localStorage.autoNext_STATUS == '1')
-	{
-		chbAutoNext.checked = 1;
-		game.hostStart();
-	}
+if (localStorage.autoNext_STATUS == '1'){
+	chbAutoNext.checked = 1;
+	game.hostStart();
 }
 
+var scores = document.getElementById('userpanel-scores-container' );
+var observer = new MutationObserver(CheckRating);
+observer.observe(scores, { childList: true, subtree: true, characterData: true });
+
+function CheckRating(){
+	var rating = document.querySelector(".player.you .rating" );
+	if (rating === true && rating.style.display !== 'none' && localStorage.autoNext_STATUS == '1') {
+		return false;
+	}
+	observer.disconnect();
+	setTimeout(GoToNext, 1000);
+}
+
+function GoToNext() {
+	if (localStorage.autoNext_STATUS == '1')
+		window.location='/g/' + document.location.href.substr(29, 5) + '.replay';
+}
 function chbChanged() {
 	if (chbAutoNext.checked) 	{
 		localStorage.autoNext_STATUS = '1';
@@ -36,26 +48,5 @@ function chbChanged() {
 	}
 	else {
 		localStorage.autoNext_STATUS = '0';
-		waitingForNext = false;
 	}
 }
-
-document.getElementById("userpanel-scores-container").addEventListener("DOMSubtreeModified", CheckRating);
-
-function CheckRating(){
-	var rating = document.querySelector(".player.you .rating");
-	if (localStorage.autoNext_STATUS == '1' &&
-		rating !== null && rating.innerText.match(' место ') !== null &&
-		waitingForNext === false)
-	{
-		waitingForNext = true;
-		setTimeout(GoToNext, 2000);
-	}
-}
-
-function GoToNext() {
-	if (localStorage.autoNext_STATUS == '1')
-		window.location='/g/' + document.location.href.substr(29, 5) + '.replay';
-}
-
-
