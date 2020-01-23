2e87db3aa25ecb8d788ea41d6512c65cf9280fd4
Akmat ali <sirazhidinovakmatali@gmail.com>
2020-01-23 22:56:43 +0600

    Добавляет чекбокс авторестарт (#104)
    Добавлена настройка "авто" для автоматического создания нового заезда в случае превышения установленного лимита на количество ошибок.

--- scripts/KG_StopErrorsRace.user.js
+++ scripts/KG_StopErrorsRace.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         KG_StopErrorsRace
 // @namespace    klavogonki
-// @version      1.0.1
+// @version      1.1.1
 // @description  Останавливает заезд и создает новый если количество ошибок больше, чем указанное в настройках.
 // @author       Akmat
 // @include      http://klavogonki.ru/g/*
@@ -9,40 +9,76 @@
 
 "use strict";
 
+function addStylesForFailRaceText() {
+    const css = `.stop-error-fail {
+        color: red;
+        border-radius: 3px;
+        font-size: 49px;
+        font-weight: bold;
+        text-align: center;
+        border: 2px solid red;
+        padding: 20px 20px;}`,
+    head = document.head || document.getElementsByTagName('head')[0],
+    style = document.createElement('style');
+    head.appendChild(style);
+
+    style.type = 'text/css';
+    if (style.styleSheet){
+        // This is required for IE8 and below.
+        style.styleSheet.cssText = css;
+    } else {
+        style.appendChild(document.createTextNode(css));
+    }
+}
+
 function createElements() {
-	const is_on = localStorage.selectedItem !== 'off';
+	const is_on = localStorage.getItem("selectedItem") !== 'off';
 	const div    = document.createElement("div");
 	const select = document.createElement("select");
 	const label  = document.createElement("label");
+	const checkboxInput  = document.createElement("input");
+	const checkboxLabel  = document.createElement("label");
 	
-	label.textContent = 'Выберите количество ошибок';
+	label.textContent = 'Лимит ошибок:';
+	checkboxLabel.innerHTML = 'Авто&#8594;';
+	checkboxLabel.htmlFor = 'checkboxInputId';
 	select.name = 'stopErrorsSelect';
 	label.id = 'stopLabelId';
+
 	div.id = 'stopSelectedDivId';
 	select.id = 'stopSelectId';
+	checkboxInput.id = 'checkboxInputId';
+	checkboxInput.type = 'checkbox';
 
+	checkboxInput.checked = !!localStorage.getItem("autoNextErrorCheckbox");
+	
 	const node = document.getElementsByClassName("rc");
 	node[2].appendChild(div);
 	div.appendChild(label);
 	div.appendChild(select);
-	label.style.margin = '0 5px 0 0';
+	div.appendChild(checkboxInput);
+	div.appendChild(checkboxLabel);
+	checkboxInput.style.margin = '0 0 0 15px';
+	label.style.margin = '0 31px 0 0';
 
 	let option = document.createElement("option");
-	
+
 	if (!is_on) {
 		option.selected = true;
+		checkboxInput.disabled = true;
 	}
 	option.innerText = 'off';
 	
 	select.appendChild(option);
 
 
-	for (let i = 1; i < 6; i++) {
+	for (let i = 0; i < 6; i++) {
 		option = document.createElement("option");
 		option.value = i;
 		option.innerText = i;
 
-		if (localStorage.selectedItem !== 'off' && +localStorage.selectedItem === i) {
+		if (localStorage.getItem("selectedItem") !== 'off' &&
+		 +localStorage.getItem("selectedItem") === i) {
 			option.selected = true;
 		}
 		select.appendChild(option);
@@ -53,12 +89,25 @@ function checkSelect() {
 	document.getElementById("stopSelectId")
 	.addEventListener("change", (e) => {
 		const value = e.target.selectedOptions[0].textContent;
-		localStorage.selectedItem = value;
+		localStorage.setItem("selectedItem", value);
+		document.getElementById("checkboxInputId").disabled = value === 'off';
 	});
 }
 
-if (localStorage.selectedItem === undefined) {
-	localStorage.selectedItem = 'off';
+function checkInputAutoNext() {
+	document.getElementById("checkboxInputId")
+	.addEventListener("change", (e) => {
+		localStorage.setItem("autoNextErrorCheckbox", (e.target.checked) ? 1 : '');
+	});
+}
+
+
+if (localStorage.getItem("selectedItem") === undefined) {
+	localStorage.setItem("selectedItem", "off");
+}
+
+if (localStorage.getItem("autoNextErrorCheckbox") === undefined) {
+	localStorage.setItem("autoNextErrorCheckbox", 1);
 }
 
 function is_competition() {
@@ -78,18 +127,41 @@ function createNewGameAndRedirect() {
 	window.location = `/g/${url}.replay`;
 }
 
+function creatFailWord() {
+    const p = document.createElement("p");
+    p.innerText = 'Провал';
+    p.setAttribute( 'class', 'stop-error-fail');
+    
+    if (document.getElementsByClassName('stop-error-fail').length === 0) {
+        const textRace = document.getElementById('typetext');
+        textRace.style.color = '#ccc';
+        textRace.appendChild(p);
+    }
+}
+
 window.addEventListener("load", () => {
 	if (is_competition()) {
 		createElements();
 		checkSelect();
+		addStylesForFailRaceText();
+		checkInputAutoNext();
 
 		const errors = document.getElementById("errors-label");
 
 		window.addEventListener("keyup", (event) => {
-			if (localStorage.selectedItem !== 'off') {
-				if (errors.innerText > localStorage.selectedItem) {
+			if (localStorage.getItem("selectedItem") !== 'off') {
+				if (localStorage.getItem("selectedItem") === "0" &&
+					errors.innerText === "1") {
 					stopGame();
+					creatFailWord();
 					createNewGameAndRedirect();
+				}
+
+				if (errors.innerText > localStorage.getItem("selectedItem")) {
+					stopGame();
+					creatFailWord();
+					if (localStorage.getItem("autoNextErrorCheckbox"))
+						createNewGameAndRedirect();
 				}
 			}
 		});
