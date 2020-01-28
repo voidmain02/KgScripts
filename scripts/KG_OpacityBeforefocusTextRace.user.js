4e3d8eb14b67e56ee3ae5dd7b24b95be54df4f14
Akmat <sirazhidinovakmatali@gmail.com>
2020-01-28 16:28:57 +0600

    Изменение №1

--- scripts/KG_OpacityBeforefocusTextRace.user.js
+++ scripts/KG_OpacityBeforefocusTextRace.user.js
@@ -2,35 +2,47 @@
 // @name         KG_OpacityBeforefocusTextRace
 // @namespace    klavogonki
 // @version      1.0.0
-// @description  Набранный текст в текушей игре будет выглядет серым цветом тем самым помогает легко ориентироваться в тексте когда набираете.
+// @description  Набранный текст в текущей игре будет отображаться серым цветом (помогает легко ориентироваться в тексте).
 // @author       Akmat
 // @include      http://klavogonki.ru/g/*
 // ==/UserScript==
 
 "use strict";
 
+function injectionCss() {
+	const style = document.createElement('style');
+	style.textContent = "#beforefocus { color: #cccccc !important; }";
+	style.id = 'styleBeforefocus';
+	if (!document.getElementById("styleBeforefocus"))
+		document.head.appendChild(style);
+}
+
+function removeInjectionCss() {
+	const delStyleTag = document.getElementById("styleBeforefocus");
+	delStyleTag.parentNode.removeChild(delStyleTag);
+}
+
 function createElements() {
 	const div    = document.createElement("div");
 	const label  = document.createElement("label");
 	const checkboxInput  = document.createElement("input");
-
+	const node = document.querySelector('#params .rc');
+	
 	label.textContent = 'Набранный текст серый';
 	label.htmlFor = 'opacityCheckboxInputId';
-	label.id = 'opacityLabel';
 
-	div.id = 'opacityDiv';
 	checkboxInput.id = 'opacityCheckboxInputId';
 	checkboxInput.type = 'checkbox';
 
+	if (!!localStorage.getItem("opacityCheckboxStatus")) injectionCss();
 	checkboxInput.checked = !!localStorage.getItem("opacityCheckboxStatus");
 
-	const node = document.getElementsByClassName("rc");
-	node[2].appendChild(div);
+	node.appendChild(div);
 	div.appendChild(checkboxInput);
 	div.appendChild(label);
 }
 
-function is_competition() {
+function is_gameRace() {
 	const rightUrl = window.location.href;
 	return !!rightUrl.match(/gmid/);
 }
@@ -38,24 +50,19 @@ function is_competition() {
 function checkInputOpacity() {
 	document.getElementById("opacityCheckboxInputId")
 	.addEventListener("change", (e) => {
-		localStorage.setItem("opacityCheckboxStatus", (e.target.checked) ? 1 : '');
+		if (e.target.checked) {
+			injectionCss();
+			localStorage.setItem("opacityCheckboxStatus", 1);
+		} else {
+			removeInjectionCss();
+			localStorage.setItem("opacityCheckboxStatus", "");
+		}
 	});
 }
 
-if (localStorage.getItem("opacityCheckboxStatus") === undefined) {
-	localStorage.setItem("opacityCheckboxStatus", 1);
-}
-
 window.addEventListener("load", () => {
-	if (is_competition()) {
+	if (is_gameRace()) {
 		createElements();
 		checkInputOpacity();
-
-		window.addEventListener("keyup", () => {
-			const beforeFocus = document.getElementById("beforefocus");
-			if (beforeFocus && +localStorage.getItem("opacityCheckboxStatus")) {
-				beforeFocus.style.color = "#ccc";
-			}
-		});
 	}
 });
