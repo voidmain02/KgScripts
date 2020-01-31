a74fd3bf1f1a871f3e3fc0d2e95c8ac997a5621a
SA <sirazhidinovakmatali@gmail.com>
2020-01-31 10:35:30 +0600

    Набранный текст будет отображаться серым(прозрачным цветом) (#108)
    * Уже набранный текст в заезде будет отображаться серым цветом

---
+++ scripts/KG_OpacityBeforefocusTextRace.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         KG_OpacityBeforefocusTextRace
+// @namespace    klavogonki
+// @version      1.0.0
+// @description  Набранный текст в текущей игре будет отображаться серым цветом (помогает легко ориентироваться в тексте).
+// @author       Akmat
+// @include      http://klavogonki.ru/g/*
+// ==/UserScript==
+
+"use strict";
+
+function injectionCss() {
+	const style = document.createElement('style');
+	style.textContent = "#beforefocus { opacity: 0.1 !important; }";
+	document.head.appendChild(style);
+}
+
+function is_gameRace() {
+	const rightUrl = window.location.href;
+	return !!rightUrl.match(/gmid/);
+}
+
+window.addEventListener("load", () => {
+	if (is_gameRace()) injectionCss();
+});
